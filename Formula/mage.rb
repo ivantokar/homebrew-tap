c253740ff20dcf2f222ee488844dee2e2eaad35b
Ivan Tokar <i.j.tokar@gmail.com>
2026-07-09 17:06:27 +0300

    Install Mage from prebuilt binary archive

--- Formula/mage.rb
+++ Formula/mage.rb
@@ -1,15 +1,15 @@
 class Mage < Formula
   desc "CLI for scaffolding Clean Layered DDD architecture in Vapor projects"
   homepage "https://github.com/ivantokar/mage"
-  url "https://github.com/ivantokar/mage/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "dc822a5e420271f20b738df2b79d9ef7c8bd5e89b686054238c76192f6f907e2"
+  url "https://github.com/ivantokar/mage/releases/download/v0.1.0/mage-0.1.0-arm64-apple-darwin.tar.gz"
+  sha256 "00a2c1d1b483be871b30179f2ff684c1dc0d77c2fac69147ded914e5ad14ef6f"
   license "MIT"
 
-  depends_on "swift" => :build
+  depends_on arch: :arm64
 
   def install
-    system "swift", "build", "-c", "release", "--product", "Mage", "--disable-sandbox"
-    bin.install ".build/release/Mage" => "mage"
+    bin.install "mage"
+    bin.install "mage_Mage.bundle"
   end
 
   test do
